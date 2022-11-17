2294b495437a8d1db05e9bf9ee1dd2abbdab2b97
mj0717 <a81930875@gmail.com>
2022-11-17 16:25:25 +0900

    1

---
+++ main.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char *argv[]) {
+	
+	srand((unsigned)time(NULL));
+	
+	printf("^_^^_^^_^^_^^_^^_^^_^^_^^_^^_^^_^^_^^_^^_^\n");
+	printf(">_<>_<>_<>_<>_<>_<>_<>_<>_<>_<>_<>_<>_<>_<\n");
+	printf("                   BINGO GAME              \n");
+	printf("^_^^_^^_^^_^^_^^_^^_^^_^^_^^_^^_^^_^^_^^_^\n");
+	printf(">_<>_<>_<>_<>_<>_<>_<>_<>_<>_<>_<>_<>_<>_<\n\n\n");
+	
+	
+	
+	printf("\n\n\n\n\n\n\n\n");
+	printf("^_^^_^^_^^_^^_^^_^^_^^_^^_^^_^^_^^_^^_^^_^\n");
+	printf(">_<>_<>_<>_<>_<>_<>_<>_<>_<>_<>_<>_<>_<>_<\n");
+	printf("                 CONGRATULATION!!!         \n");
+	printf("!>@#$%^<&.,?12345(*BINGO!!!*)!<@#12345.,$%^&>*\n");
+	printf("                   YOU WIN                 \n");
+	printf("^_^^_^^_^^_^^_^^_^^_^^_^^_^^_^^_^^_^^_^^_^\n");
+	printf(">_<>_<>_<>_<>_<>_<>_<>_<>_<>_<>_<>_<>_<>_<\n");
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	return 0;
+}
